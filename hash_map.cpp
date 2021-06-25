8e9333fc4203bedd491071d491b8242db23e4721
hamellen <78523919+hamellen@users.noreply.github.com>
2021-06-25 17:57:56 +0900

    Add files via upload

---
+++ hash_map.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class hash_map {
+    
+    vector<int>data;
+public:
+    hash_map(int n) {
+        data = vector<int>(n, -1);
+    }
+    void insert(int value) {
+        int n = data.size();
+        data[value % n] = value;
+
+    }
+    bool find(int value) {
+        int n = data.size();
+        return (data[value % n] == value);
+     }
+    void erase(int value) {
+        int n = data.size();
+        if (data[value % n] == value) {
+            data[value % n] = -1;
+        }
+    }
+};
+int main()
+{
+    hash_map map(7);
+
+    auto find = [&](int value) {
+        if (map.find(value))
+            cout << "성공" << endl;
+        else
+            cout << "실패" << endl;
+    };
+    map.insert(6);
+    map.insert(5);
+    map.insert(3);
+    map.erase(6);
+    find(4);
+    return 0;
+}
