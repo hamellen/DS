ecbaf9483e8eec3ab0c845c71e94e6a2b802849a
hamellen <78523919+hamellen@users.noreply.github.com>
2021-06-26 22:53:57 +0900

    Add files via upload

---
+++ hashtable.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <unordered_map>//key,value 저장
+#include<unordered_set>//key 저장
+using namespace std;
+
+void print(const unordered_set<int>& container) {
+	for (const auto& element : container)
+		cout << element << ' ';
+}
+void print(const unordered_map<int,int>& container) {
+
+	for (const auto& element : container)
+		cout << element.second<<" "<< element.first << ' ';
+}
+void find(const unordered_set<int>& container, int value) {
+	if (container.find(value) != container.end())
+		cout << "검색 성공" << endl;
+	else
+		cout << "검색실패" << endl;
+}
+void find(const unordered_map<int,int>& container, int value) {
+	if (container.find(value) != container.end())
+		cout << "검색 성공" << endl;
+	else
+		cout << "검색실패" << endl;
+}
+int main()
+{
+   
+}
+
